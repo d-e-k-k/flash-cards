aeac8692cf7067834eb8cf59e2e36adfee7dbdd8
d-e-k-k <davideberhart8@gmail.com>
2020-10-13 09:51:02 -0400

    Got card list drop down  working

--- complete-cards-modal.js
+++ complete-cards-modal.js
@@ -13,4 +13,12 @@ const cardListButton = document.querySelector('#card-list-button');
 cardListButton.addEventListener('click', (event) => {
     event.preventDefault();
     containerCardList.classList.toggle('hidden');
+    const cardListUl = document.querySelector('#cards-list-ul');
+    cardListUl.innerHTML = '';
+    for(let i = 0; i < cards.length; i++){
+        const cardTitle = document.createElement('p');
+        cardTitle.textContent = cards[i].title;
+        cardTitle.classList.add('card-title');
+        cardListUl.appendChild(cardTitle);
+    }
 })
