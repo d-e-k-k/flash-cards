133a78836018a8fb28da1c7e537195bda2e55bfc
d-e-k-k <davideberhart8@gmail.com>
2020-10-13 14:05:53 -0400

    Have the random picker working with a conditional and recursion. It has a limit of 10 attempts to get a random word each time the button is pressed. If it doesnt it will just create an error and the console but can be reclicked for another attempt.

--- random-vocab.js
+++ random-vocab.js
@@ -1,7 +1,41 @@
+
+let stopper = 0;
 const randomVocabButton = document.querySelector('#random-vocab-button');
 
 randomVocabButton.addEventListener('click',(event) => {
+    stopper = 0;
     event.preventDefault();
+    fetchRandomWord();
+    
+
+    // fetch('https://rapidapi.p.rapidapi.com/words/?random=true', {
+	// 		method: 'GET',
+	// 		headers: {
+	// 			'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
+	// 			'x-rapidapi-key': 'c7654cee50mshc2f4b12e9dcbe68p1ce2b6jsn773f7721b1db',
+	// 		},
+    //     })
+    // .then(res => res.json())
+    // .then(resJson => { 
+    //     console.log(resJson);
+    // //     console.log(resJson.results[0].definition);
+
+    //     if (resJson.results[0].definition){
+    //         console.log('there is a def for this word');
+    //         cards.push({
+    //             title: resJson.word,
+    //             def: resJson.results[0].definition,
+    //             completed: false,
+    //         });
+    //     updateTotalAmountOfCards();
+    //     }
+    //     console.log(resJson);
+})
+// })
+
+let internalStopper = 0;
+// Attribution: Rapidapi had a template for the headers that I used.
+function fetchRandomWord(){
     fetch('https://rapidapi.p.rapidapi.com/words/?random=true', {
 			method: 'GET',
 			headers: {
@@ -11,10 +45,14 @@ randomVocabButton.addEventListener('click',(event) => {
         })
     .then(res => res.json())
     .then(resJson => {
-        console.log(resJson);
-        console.log(resJson.results[0].definition);
-        if (resJson.results[0].definition){
-            console.log('there is a def for this word');
+        if(resJson.results == undefined & stopper < 10){
+            console.log('no def available');
+            stopper ++; 
+            console.log(stopper);
+            fetchRandomWord();
+        }else{
+            console.log('def available');
+            console.log(resJson.results[0].definition);
             cards.push({
                 title: resJson.word,
                 def: resJson.results[0].definition,
@@ -22,8 +60,5 @@ randomVocabButton.addEventListener('click',(event) => {
             });
         updateTotalAmountOfCards();
         }
-        console.log(resJson);
     })
-})
-
-// Attribution: Rapidapi had a template for the headers that I used.
+}
